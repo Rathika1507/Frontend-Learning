ba4205d0fea2c4781be3123068df82b05f5b9ae7
rathika <rathika@gmail.com>
2025-06-18 14:08:41 +0530

    created java script file

---
+++ Basics/js learn/demo.js
@@ -0,0 +1,139 @@
+function iphone()
+{
+    console.log("This is iphone")
+}
+iphone()
+
+function Redmi()
+{
+    console.log("This is Redmi")
+}
+Redmi()
+
+function oppo()
+{
+    console.log("This is oppo")
+}
+oppo()
+
+var a=10
+var b=20
+function add()
+{
+    console.log(a+b)
+}
+add()
+
+var factor="Kamal"
+var fplayer="Dhoni"
+var fmovie="Friends"
+ 
+function Favourite()
+{
+ console.log("Favourite Actor:"+factor)
+ console.log("Favourite Player:"+fplayer)
+ console.log("Favourite movie:"+fmovie)
+}
+Favourite()
+
+function add(a,b)
+{
+    console.log(a+b)
+}
+add(10,20)
+
+function area(l,b)
+{
+    var a=l*b
+    console.log("Area is:"+a)
+}
+var length=10
+var breadth=20
+area(20,10)
+
+
+function myname()
+{
+    return"ram"
+}
+var a=myname()
+console.log(a)
+
+function add(a,b)
+{
+    return a+b
+}
+var total=add(10,20)
+console.log(total)
+
+
+
+var finshhomework=false
+ 
+if(finshhomework)
+{
+    console.log("Great Job")
+}
+else{
+    console.log("Finish your homework befor playing")
+}
+
+
+console.log(false && true && true)
+console.log( false || false|| true)
+
+
+
+var season="summer"
+
+if (season =="spring")
+{
+ console.log("Enjoy the blomming flower")
+}
+
+if (season =="summer")
+{
+ console.log("Have fun in the sun")
+}
+
+if (season =="autumn")
+{
+ console.log("Admire the colorful leaves")
+}
+
+if (season =="winter")
+{
+ console.log("Bundle up and stay warm")
+}
+
+
+for(count=1;count<=5;count=count+1)
+{
+    console.log("rathi")  
+}
+
+
+
+for (count=1;count<=10;count=count+1)
+{
+    console.log(count)
+}
+
+
+for (count=1;count<=10;count=count+2)
+{
+    console.log(count)
+}
+
+
+for (count=10;count>=1;count=count-1)
+{
+    console.log(count)
+}
+
+for (count=1;count<=10;count=count+1)
+{
+    console.log(count+"*2="+(count*2))
+}
+
+
