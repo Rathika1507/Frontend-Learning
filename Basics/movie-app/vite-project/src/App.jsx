002c7e65f1654dcdbb72c6fead1865f0437417fc
rathika <rathika@gmail.com>
2025-06-23 14:38:29 +0530

    added movie card and trending list

--- Basics/movie-app/vite-project/src/App.jsx
+++ Basics/movie-app/vite-project/src/App.jsx
@@ -1,105 +1,134 @@
-import { useEffect,useState } from "react"
-import Search from "./components/Search.jsx"
-import Spinner from './components/Spinner.jsx';
+import { useEffect, useState } from 'react'
+import Search from './components/Search.jsx'
+import Spinner from './components/Spinner.jsx'
 import MovieCard from './components/MovieCard.jsx'
 import { useDebounce } from 'react-use'
-import { updateSearchCount } from "./appWrite.js";
+import { getTrendingMovies, updateSearchCount } from './appWrite.js'
 
- 
-const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
   method: 'GET',
-  headers:{
-    accept:'application/json',
-    Authorization:`Bearer ${API_KEY}`
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
   }
 }
 
-
 const App = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [movieList,setMovieList] = useState([]);
-    const [isLoading,setIsLoading] = useState(false);
-    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('');
 
-    useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+  const [movieList, setMovieList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-    const fetchMovies  = async (query ='') => {
-      setIsLoading(true);
-      setErrorMessage('');
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
-      try{
-         const endpoint = query 
-         ?`${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-         :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  // Debounce the search term to prevent making too many API requests
+  // by waiting for the user to stop typing for 500ms
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
 
-         const response = await fetch(endpoint,API_OPTIONS);
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true);
+    setErrorMessage('');
 
-        if(!response.ok){ 
-         throw new Error('Failed to fetch movies');
-        }
+    try {
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
-         const data = await response.json();
+      const response = await fetch(endpoint, API_OPTIONS);
 
-        if(data.Response == 'False'){
-          setErrorMessage(data.Error || 'Failed to fetch movie');
-          setMovieList([]);
-          return;
-        }
-        
-        setMovieList(data.results ||[]);
-
-        updateSearchCount();
-      } catch(error){
-        console.error(`Error fetching movies: ${error}`);
-        setErrorMessage('Error fetching movies. Please try again later.');
-
-      }finally{
-        setIsLoading(false);
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies');
       }
 
+      const data = await response.json();
+
+      if(data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later.');
+    } finally {
+      setIsLoading(false);
     }
+  }
 
-    
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
+    }
+  }
+
   useEffect(() => {
-    fetchMovies(searchTerm);
-  }, [searchTerm]);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
-     
-  return(
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
+  return (
     <main>
-      <div className="pattern" />
+      <div className="pattern"/>
 
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="Hero Banner" />
-          <h1>Find <span className="text-gradient">Movies</span> you'll Enjoy Without the Hassle</h1>
-        
-           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-      <section className="all-movies">
-        <h2 className="mt-[40px]">All Movies</h2>
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
 
-      {isLoading?(
-        <Spinner />
-      ):errorMessage ? (
-        <p className="text-red-500">{errorMessage}</p>
-      ):(
-        <ul>
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
               {movieList.map((movie) => (
                 <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
-      </section>
+        </section>
       </div>
     </main>
-    
   )
 }
 
